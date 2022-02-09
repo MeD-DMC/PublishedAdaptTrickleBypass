673cfeb078ec55c23b6f142a5c1659dcabee90b2
ftourangeau <francis.tourangeau@hrsdc-rhdcc.gc.ca>
2022-02-09 16:28:59 -0500

    added removal of disabled state on trickle button

--- trickle_bypass.js
+++ trickle_bypass.js
@@ -3,6 +3,7 @@ var loop_length = trickle_buttons.length;
 var loop_count = 0;
 trickle_buttons.each(function(){   
     loop_count++;
+    $(this).removeAttr('disabled');
     this.click();
     if(loop_count === loop_length){
         trickleDone();
